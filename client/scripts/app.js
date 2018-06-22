50abf3cb17c314cc6f8a90e473d0b5fc56d09af3
JP Vertil <vertil.jp@gmail.com>
2018-06-22 12:27:21 -0700

    Creation of other functions, but empty for now

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,30 +8,50 @@ app.init = function () {
 
 app.send = function (message) {
     
-    var message = {
-      username: message.username,
-      text: message.text,
-      roomname: message.roomname
-     };
+  var message = {
+    username: message.username,
+    text: message.text,
+    roomname: message.roomname
+  };
 
 
-	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
-	  type: 'POST',
-	  data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-	  console.log('chatterbox: Message sent');
-	  },
-	  error: function (data) {
-	  // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	  console.error('chatterbox: Failed to send message', data);
-	  }
-	});
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
 
 
 app.fetch = function () {
-	
-}
+  $.ajax({
+    
+
+
+  });
+
+app.clearMessages = function () {
+
+  };
+
+
+app.renderMessage = function () {
+
+  };
+
+
+app.renderRoom = function () {
+
+  };
+
+ 
+};
